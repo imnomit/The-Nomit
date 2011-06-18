cd7d63ac1d55e025cb77e90a4ba3a779aa633e7f
Timon Gerber <im.nomit@facebook.com>
2011-06-17 17:13:39 -0700

    First commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DEVELOPMENT_ENVIRONMENT', true);
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'k23025_1');
+define('DB_PASSWORD', '4dfbeb2987daf');
+define('DB_NAME', 'k23025_1');
